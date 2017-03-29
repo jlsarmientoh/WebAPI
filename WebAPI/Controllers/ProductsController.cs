423cb05a4d1dd92113df9d62fcf5163fb11dcd62
Jorge Luis Sarmiento Herrera <yorchsarmiento@gmail.com>
2017-03-28 23:42:23 -0500

    Added CORS

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -7,12 +7,15 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using System.Web.Http.Description;
 using WebAPI.Models;
 using WebAPI.Service;
 
+
 namespace WebAPI.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class ProductsController : ApiController
     {
         private ProductService productService = new ProductService();
