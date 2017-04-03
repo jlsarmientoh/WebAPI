5e32bcc62715fc80da5a423d5572ba0ee53d3950
Jorge Luis Sarmiento Herrera <yorchsarmiento@gmail.com>
2017-04-03 03:05:35 -0500

    Added Serializable to Exceptions

--- WebAPI/Exceptions/CreateProductException.cs
+++ WebAPI/Exceptions/CreateProductException.cs
@@ -5,6 +5,7 @@ using System.Web;
 
 namespace WebAPI.Exceptions
 {
+    [Serializable]
     public class CreateProductException : Exception
     {
         public CreateProductException(string message) : base(message)
--- WebAPI/Exceptions/GetProductsException.cs
+++ WebAPI/Exceptions/GetProductsException.cs
@@ -5,6 +5,7 @@ using System.Web;
 
 namespace WebAPI.Exceptions
 {
+    [Serializable]
     public class GetProductsException : Exception
     {
         public GetProductsException(string message) : base(message)
